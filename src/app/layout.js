a9fa5ee7ed453f0dc724fd37577708a35986c32d
thecyberjerry <himanshusharma2719@gmail.com>
2024-07-24 11:21:51 +0530

    fonts-update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,4 @@
-import { Inter } from "next/font/google";
 import "./scss/mains.scss";
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -10,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} `}>{children}</body>
+      <body >{children}</body>
     </html>
   );
 }
